084be6bcc1ba8df5e16df4e57693246ba552bec2
FranciscoCai <jiehaocai54@gmail.com>
2025-01-25 00:20:21 +0100

    sss

--- Assets/Script/TriggerVolume.cs
+++ Assets/Script/TriggerVolume.cs
@@ -4,10 +4,12 @@ using UnityEngine.Events;
 public class TriggerVolume : MonoBehaviour
 {
     public UnityEvent<GameObject> OnEnter;
+    private bool HasEnter = false;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out CharacterController player))
+        if(other.TryGetComponent(out CharacterController player) && !HasEnter)
         {
+            Debug.Log(111);
             OnEnter.Invoke(player.gameObject);
         }
     }
