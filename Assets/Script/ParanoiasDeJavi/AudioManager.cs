72ccd7ca8f569cc895322e4a7310e2e78d43ae6e
Javier495 <9204644@alumnos.ufv.es>
2025-01-25 17:27:27 +0100

    Audio Manager

---
+++ Assets/Script/ParanoiasDeJavi/AudioManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CrossfadeAudio(AudioSource fromAudio, AudioSource toAudio, float fadeOutSpeed, float fadeInSpeed)
+    {
+        StartCoroutine(FadeOutIn(fromAudio, toAudio, fadeOutSpeed, fadeInSpeed));
+    }
+
+    private IEnumerator FadeOutIn(AudioSource fromAudio, AudioSource toAudio, float fadeOutSpeed, float fadeInSpeed)
+    {
+        float fromStartVolume = fromAudio.volume;
+        float toStartVolume = toAudio.volume;
+
+        float fromTime = 0f;
+        float toTime = 0f;
+
+        toAudio.Play(); // Asegurar que el nuevo audio comienza a reproducirse
+
+        while (fromAudio.volume > 0 || toAudio.volume < 1)
+        {
+            if (fromAudio.volume > 0)
+            {
+                fromTime += Time.deltaTime / fadeOutSpeed;
+                fromAudio.volume = Mathf.Lerp(fromStartVolume, 0, fromTime);
+            }
+
+            if (toAudio.volume < 1)
+            {
+                toTime += Time.deltaTime / fadeInSpeed;
+                toAudio.volume = Mathf.Lerp(toStartVolume, 1, toTime);
+            }
+
+            yield return null; // Espera al siguiente frame
+        }
+
+        fromAudio.Stop(); // Detener el audio antiguo cuando ya no se escucha
+    }
+
+    public void playAudio(AudioSource clip)
+    {
+        clip.Play();
+    }
+}
