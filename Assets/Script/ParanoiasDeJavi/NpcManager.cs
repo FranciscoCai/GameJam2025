0d83b9943bc5500b13e1fa952f680e3ed594330b
Javier495 <9204644@alumnos.ufv.es>
2025-01-25 14:01:12 +0100

    cambios

---
+++ Assets/Script/ParanoiasDeJavi/NpcManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class NpcManager : MonoBehaviour
+{
+    public static NpcManager Instance;
+    public float timeUntilAttack;
+    public float countdownDuration;
+    public float pumpDuration;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
